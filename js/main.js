9c8fd67a40d9ec2c6e8a7129e4204b0a2b4d6177
Gemma Whiting <135684832+crowdisciple@users.noreply.github.com>
2025-05-22 03:26:54 +1000

    Reapply "Added Art Section"
    This reverts commit e0c6ed28e49282e50b23f93c10dcd1d22c38d45d.

--- js/main.js
+++ js/main.js
@@ -13,5 +13,156 @@ function openMenu() {
 
 function closeMenu() {
     mobileMenu.style.display = "none";
-    body.style.overflowY = "scroll";
+    body.style.overflowY = "auto";
+}
+
+// Get the modal
+var modal = document.getElementById("artModal");
+
+var img = document.getElementById("art0Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art1Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art2Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art3Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art4Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art5Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art6Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art7Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art8Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art9Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art10Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art11Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art12Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+
+var img = document.getElementById("art13Img");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+  body.style.overflowY = "hidden";
+}
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  body.style.overflowY = "scroll";
 }
